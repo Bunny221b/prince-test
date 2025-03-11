b92cc840e7a9eec0b0660d2fbb0631a87b107942
Bunny221b <arpit.tabs17@gmail.com>
2025-03-11 14:31:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex justify-center items-center bg-black text-white h-[100vh]'>
+      <h1>Helloo</h1>
+    </div>
+  );
+}
+
+export default App;
